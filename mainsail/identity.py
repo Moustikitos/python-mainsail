44021df8c44b5348676c84f48fa0f5666ef4c48a
Moustikitos <moustikitos@gmail.com>
2024-03-23 18:02:34 +0100

    refactor: moved sign from transaction module to identity module

--- mainsail/identity.py
+++ mainsail/identity.py
@@ -6,6 +6,7 @@ import binascii
 import cSecp256k1
 
 from mainsail import cfg
+from typing import Union
 
 
 def combinePublicKey(*puki):
@@ -20,3 +21,24 @@ def getWallet(puk: str, version: int = None) -> str:
     seed = binascii.unhexlify(f"{version or cfg.version:02x}") + ripemd160
     b58 = base58.b58encode_check(seed)
     return b58.decode('utf-8') if isinstance(b58, bytes) else b58
+
+
+def sign(
+    data: Union[str, bytes], prk: Union[cSecp256k1.Bcrpt410, str, int] = None
+) -> str:
+    """
+    Compute raw Schnorr signature from data using private key according to
+    bcrypto 4.10 spec.
+
+    Args:
+        data (str|bytes): data used for signature computation.
+        prk (str|int|cSecp256k1.Bcrpt410): private key or keyring.
+
+    Returns:
+        str: Schnorr raw signature (ie r | s)
+    """
+    if not isinstance(prk, cSecp256k1.Bcrpt410):
+        prk = cSecp256k1.Bcrpt410(prk)
+    return prk.sign(
+        data.encode("utf-8") if isinstance(data, str) else data
+    ).raw()
